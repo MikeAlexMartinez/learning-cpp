0aac034bebca1d8f23d5c66d5f335891b9334dda
Michael Martinez <mike@Michaels-Mac-mini.local>
2019-09-22 21:40:15 +0100

    got hello world to run on macbook with brew g++-9

---
+++ ch1/headers/std_lib_facilities.h
@@ -0,0 +1,13 @@
+//
+//  std_lib_facilities.h
+//  ch1
+//
+//  Created by Michael Martinez on 22/09/2019.
+//  Copyright © 2019 Michael Martinez. All rights reserved.
+//
+
+#ifndef std_lib_facilities_h
+#define std_lib_facilities_h
+
+
+#endif /* std_lib_facilities_h */
